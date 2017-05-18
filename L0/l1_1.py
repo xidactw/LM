9860988b5a094bdcde6abc3ce1233f3845b2ba6d
xidactw <zhangdi214@126.com>
2017-05-18 17:05:12 +0800

    1

--- L0/l1_1.py
+++ L0/l1_1.py
@@ -1,15 +1,17 @@
-import matplotlib.pyplot as plt
 import pandas as pd
-import  tensorflow as tf
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
-data = pd.read_csv('t1.csv')
+data = pd.read_csv('Advertising.csv')
+
+
 x = data[['TV','Radio','Newspaper']]
 y = data['Sales']
 
 
 plt.figure(figsize=(9,12))
 plt.subplot(311) 
-plt.plot(data['TV'], y, 'ro') 
+plt.plot(data['TV'], y, 'ro')
 plt.title('TV')
 plt.grid()
 plt.subplot(312) 
@@ -17,7 +19,7 @@ plt.plot(data['Radio'], y, 'g^')
 plt.title('Radio')
 plt.grid()
 plt.subplot(313) 
-plt.plot(data['Newspaper'], y, 'b*') 
+plt.plot(data['Newspaper'], y, 'b*')
 plt.title('Newspaper')
 plt.grid()
 plt.tight_layout()
